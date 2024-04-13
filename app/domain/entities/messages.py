ee9f3ccf2bb5e6d34c911b8ce91a034c1c28b6f9
RustamShaykhinurov <89419894+ShaykhinurovRustam@users.noreply.github.com>
2024-04-13 22:24:18 +0300

    added chat domain events

--- app/domain/entities/messages.py
+++ app/domain/entities/messages.py
@@ -2,15 +2,13 @@ from dataclasses import dataclass, field
 from datetime import datetime
 from uuid import uuid4
 
+from domain.entities.base import BaseEntity
+from domain.events.messages import NewMessageRecievedEvent
 from domain.values.messages import Text, Title
 
 
 @dataclass
-class Message:
-    oid: str = field(
-        default_factory=lambda: str(uuid4()),
-        kw_only=True
-    )
+class Message(BaseEntity):
     created_at: datetime = field(
         default_factory=datetime.now,
         kw_only=True
@@ -24,7 +22,7 @@ class Message:
         return self.oid == __value.oid
     
 @dataclass
-class Chat:
+class Chat(BaseEntity):
     oid: str = field(
         default_factory=lambda: str(uuid4()),
         kw_only=True
@@ -47,3 +45,8 @@ class Chat:
     
     def add_message(self, message: Message):
         self.messages.add(message)
+        self.register_event(NewMessageRecievedEvent(
+            message_text=message.text.as_generic_type(),
+            chat_oid=self.oid,
+            message_oid=message.oid
+        ))
--- app/tests/domain/values/test_messages.py
+++ app/tests/domain/values/test_messages.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import pytest
 
+from domain.events.messages import NewMessageRecievedEvent
 from domain.exceptions.messages import TextTooLongException
 from domain.values.messages import Text, Title
 from domain.entities.messages import Chat, Message
@@ -42,3 +43,28 @@ def test_add_message_to_chat():
     chat.add_message(message=message)
     
     assert message in chat.messages
+    
+def test_new_message_events():
+    text = Text('Hello world')
+    message = Message(text=text)
+    
+    title = Title("Title")
+    chat = Chat(title=title)
+    
+    chat.add_message(message=message)
+    events = chat.pull_events()
+    pulled_events = chat.pull_events()
+    
+    
+    assert not pulled_events
+    assert len(events) == 1, events
+    
+    new_event = events[0]
+    
+    assert isinstance(new_event, NewMessageRecievedEvent), new_event
+    assert new_event.message_oid == message.oid
+    assert new_event.message_text == message.text.as_generic_type()
+    assert new_event.chat_oid == chat.oid
+    
+    
+    
